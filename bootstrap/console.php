bd46d76570568e3167fbcf838546232d7f19ef4d
pokoot <pokoot@gmail.com>
2016-06-26 23:42:50 +0800

    Add migration commands

--- bootstrap/console.php
+++ bootstrap/console.php
@@ -5,17 +5,22 @@ use Groovey\Migration\Adapters\Adapter;
 use Groovey\Migration\Adapters\Mysql;
 use Groovey\Migration\Migration;
 
-
-
-$console = new Application('Groovey', '1.0.0');
+$console = new Application($app['config']->get('app.name'), '1.0.0');
 $console->setDispatcher($app['dispatcher']);
 
-
-
-
 $console->addCommands([
+
+            // Migrations
             new Groovey\Migration\Commands\About(),
             new Groovey\Migration\Commands\Init($app),
+            new Groovey\Migration\Commands\Reset($app),
+            new Groovey\Migration\Commands\Listing($app),
+            new Groovey\Migration\Commands\Status($app),
+            new Groovey\Migration\Commands\Create($app),
+            new Groovey\Migration\Commands\Up($app),
+            new Groovey\Migration\Commands\Down($app),
+
+
 
         ]);
 
